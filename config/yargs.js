4382faac28cf06a1c76a130f20f0c89e39eeed60
unknown <carlos.cruz13198@gmail.com>
2020-03-07 20:41:48 -0500

    4 ejercicio

---
+++ config/yargs.js
@@ -0,0 +1,25 @@
+const argv = require(`yargs`)
+    .command(`crear`, `Crea un elemento por hacer`, {
+        descripcion: {
+            demand: true,
+            alias: `d`,
+            des: `Descripcion de la tarea por hacer`
+        }
+    }).command(`actualiza`, `Actualiza el estado completado de una tarea`, {
+        descripcion: {
+            demand: true,
+            alias: `d`,
+            des: `Descripcion de la tarea por hacer`
+        },
+        completado: {
+            default: true,
+            alias: `c`,
+            desc: `Marca como completado o pendiente la tarea`
+        }
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
